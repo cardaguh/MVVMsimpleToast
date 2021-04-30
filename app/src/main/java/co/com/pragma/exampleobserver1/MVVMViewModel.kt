e9718132fe3f04016467d271a29375cade88073a
carlos.agudelo <carlos.agudelo@pragma.com.co>
2021-04-30 09:27:32 -0500

    change livedata mutablelivedata

--- app/src/main/java/co/com/pragma/exampleobserver1/MVVMViewModel.kt
+++ app/src/main/java/co/com/pragma/exampleobserver1/MVVMViewModel.kt
@@ -1,19 +1,26 @@
 package co.com.pragma.exampleobserver1
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MVVMViewModel: ViewModel() {
 
-    val showMessage = MutableLiveData<Boolean>()
+    /*val showMessage = MutableLiveData<Boolean>()*/
+
+    private val _showMessage = MutableLiveData<Boolean>()
+    val showMessage: LiveData<Boolean>
+    get() = _showMessage
 
     fun startShowMessage() {
-        showMessage.value = true
+        _showMessage.value = true
+        //showMessage.value = true
         //showMessage.postValue(true)
     }
 
     fun endShowMessage() {
-        showMessage.value = null
+        _showMessage.value = null
+        //showMessage.value = null
         //showMessage.postValue(null)
     }
 }
